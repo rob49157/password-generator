9b2cf7423d0251de52ea4347babf57397b26a836
roberto <robertopaipo@hotmail.com>
2021-09-14 20:27:41 -0400

    loop

---
+++ script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.getElementById("generate");
+var characters = "abcdefghijkmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ1234567890!@#$%^&*()_+}{"
+var password = document.getElementById('password')
+// Write password to the #password input
+
+function generatepassword(){
+  var newpassword = " "
+    for (i = 0; i <10; i++) {
+      newpassword += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    password.value = newpassword
+   
+  }
+
+
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", function () {generatepassword()})
+    // var newpassword = " "
+    // for (i = 0; i < 13; i++) {
+    //   newpassword += characters.charAt(Math.floor(Math.random() * characters.length));
+    // }
+    // password.value = newpassword
+    
+    // 
